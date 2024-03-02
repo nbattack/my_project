bdffec8d80c5d4e70ec2e10e3bdc2b039e0cb693
nbattack <guaramatonick@gmail.com>
2024-03-02 13:06:49 -0500

    initial commmit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # my_project
+La presente aplicación sirve con el proposito de consultar/mostrar el conjunto de datos de anuncios de ventas de vehiculos.
+
+Proporciona la posibilidad de consultar los datos a través de un histograma o un diagrama de dispersión
---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+import pandas as pd 
+import plotly.express as px 
+car_data = pd.read_csv("C:/Users/Daye Seijas/my_project/vehicles_us.csv")
+
+st.header('Información de Venta de Vehiculos')
+
+button=st.button('Histograma')
+if button:
+    st.write('Histograma de conjunto de datos de anuncios de ventas de autos')
+    fig=px.histogram(car_data,x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+button_2=st.button('Dispersión')
+if button_2:
+    st.write('Diagrama de dispersión de datos')
+    fig_2=px.scatter(car_data,x='odometer')
+    st.plotly_chart(fig_2, use_container_width=True)
+
+#Casillas de verificación
+build_histogram = st.checkbox('Contruyendo histograma')
+if build_histogram:
+    st.write('Histograma de datos de anuncios de ventas de autos')
+    fig=px.histogram(car_data,x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+build_scatter = st.checkbox('Construyendo dispersión')
+if build_scatter:
+    st.write('Diagrama de dispersión de datos')
+    fig_2=px.scatter(car_data, x='odometer')
+    st.plotly_chart(fig_2, use_container_width=True)
